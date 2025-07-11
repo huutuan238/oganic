33ed55942e4282ac3e7296ebaf158ad9c08586e6
TuanNH <tuannh@gmai.com>
2025-07-11 15:59:05 +0900

    feat: show in product page

--- src/main/java/com/oganic/oganic/product/ProductController.java
+++ src/main/java/com/oganic/oganic/product/ProductController.java
@@ -7,20 +7,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.oganic.oganic.category.Category;
+import com.oganic.oganic.category.CategoryService;
+
 
 @Controller
 public class ProductController {
 
 	private final ProductService productService;
+	private final CategoryService categoryService;
 
-	ProductController(ProductService productService) {
+	ProductController(ProductService productService, CategoryService categoryService) {
 		this.productService = productService;
+		this.categoryService = categoryService;
 	}
 	
 	@GetMapping("/product")
 	public String getAllProduct(Model model) {
 		List<Product> products = productService.getProducts();
+		List<Product> latestProducts = productService.getLatestProducts();
+		List<Category> categories = categoryService.getCategories();
 		model.addAttribute("products", products);
+		model.addAttribute("latestProducts", latestProducts);
+		model.addAttribute("categories", categories);
 		return "shop-grid";
 	}
 	
