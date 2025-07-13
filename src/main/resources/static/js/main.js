296ed3d6d19892865ff05c65aaf30fd8daee342c
TuanNH <tuannh@gmai.com>
2025-07-13 20:20:10 +0900

    fix: common show product item

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -222,3 +222,25 @@
     });
 
 })(jQuery);
+
+const addCart = (productId) => {
+    const totalProductCartEle = document.getElementById("total-product-card");
+    const currentTotal = Number(totalProductCartEle.innerText);
+    totalProductCartEle.textContent = currentTotal + 1;
+    // TODO: Implement with session
+    // fetch('http://localhost:8080/cart', {
+    //     method: 'POST', 
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         productId: productId,
+    //         userId: 1,
+    //     }),
+    // })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(productId);
+    //     })
+    //     .catch(error => console.error('Error:', error));
+};
