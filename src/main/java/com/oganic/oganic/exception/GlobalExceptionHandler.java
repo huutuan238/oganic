2a1ac2ea717c7f3ba96964ab31467b604f450f1e
TuanNH <tuannh@gmai.com>
2025-07-15 15:36:21 +0900

    feat: handle exception

---
+++ src/main/java/com/oganic/oganic/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.oganic.oganic.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+        log.error("ERROR: " + ex.getMessage());
+        return new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
