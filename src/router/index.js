3eb5b368421611a1014117ab1d0b9f8ff6ead3aa
TuanNH <tuannh@gmai.com>
2025-07-16 16:02:58 +0900

    feat: split all pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,23 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
-import FromBlog from '@/components/blogs/FromBlog.vue'
 import ListProduct from '@/components/products/ListProduct.vue'
 import Contact from '@/components/contact/Contact.vue'
+import Checkout from '@/components/order/Checkout.vue'
+import ShoppingCart from '@/components/order/ShoppingCart.vue'
+import ProductDetail from '@/components/products/ProductDetail.vue'
+import BlogDetail from '@/components/blogs/BlogDetail.vue'
+import Blog from '@/components/blogs/Blog.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/shop-grid', component: ListProduct },
-  { path: '/shop-details', component: Home },
-  { path: '/shoping-cart', component: Home },
-  { path: '/checkout', component: Home },
+  { path: '/shop-details', component: ProductDetail },
+  { path: '/shoping-cart', component: ShoppingCart },
+  { path: '/checkout', component: Checkout },
   { path: '/contact', component: Contact },
-  { path: '/blog', component: FromBlog },
+  { path: '/blog', component: Blog },
+  { path: '/blog-details', component: BlogDetail },
 ]
 
 const router = createRouter({
