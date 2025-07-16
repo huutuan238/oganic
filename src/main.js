7f471eefd0b050d8668112b0ddd2b2dfe254814e
TuanNH <tuannh@gmai.com>
2025-07-16 11:47:47 +0900

    init: import static file(image, js, css)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import './assets/main.css'
+import "./assets/css/bootstrap.min.css"
+import "./assets/css/font-awesome.min.css"
+import "./assets/css/elegant-icons.css"
+import "./assets/css/nice-select.css"
+import "./assets/css/jquery-ui.min.css"
+import "./assets/css/owl.carousel.min.css"
+import "./assets/css/slicknav.min.css"
+import "./assets/css/style.css"
 
 import { createApp } from 'vue'
 import App from './App.vue'
