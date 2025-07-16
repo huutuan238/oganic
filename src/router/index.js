a7ef38442309b3a279ce74590b7d27ff2aa948c9
TuanNH <tuannh@gmai.com>
2025-07-16 15:46:13 +0900

    feat: shop grid

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
 import FromBlog from '@/components/blogs/FromBlog.vue'
+import ListProduct from '@/components/products/ListProduct.vue'
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/shop-grid', component: ListProduct },
+  { path: '/shop-details', component: Home },
+  { path: '/shoping-cart', component: Home },
+  { path: '/checkout', component: Home },
+  { path: '/contact', component: Home },
   { path: '/blog', component: FromBlog },
 ]
 
 const router = createRouter({
-  history: createWebHistory(), // ✅ for browser routing
+  history: createWebHistory(),
   routes,
 })
 
