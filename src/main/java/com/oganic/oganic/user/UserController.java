c30856741037b36a92d54ed122af6228a082c83d
TuanNH <tuannh@gmai.com>
2025-07-24 13:52:09 +0900

    format: format use controller

--- src/main/java/com/oganic/oganic/user/UserController.java
+++ src/main/java/com/oganic/oganic/user/UserController.java
@@ -2,12 +2,9 @@ package com.oganic.oganic.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,28 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.oganic.oganic.authenticate.JwtUtil;
 import com.oganic.oganic.exception.ApiResponse;
-import com.oganic.oganic.exception.ErrorResponse;
-
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/users")
 public class UserController {
-    
-    private final UserService userService;
+
+	private final UserService userService;
 
 	@Autowired
 	private JwtUtil jwtUtil;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-    
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
 	@PostMapping("/login")
 	public ResponseEntity<ApiResponse> login(@RequestBody User request) {
 		User user = userService.findByEmail(request.getEmail());
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		if (user !=null && passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+		if (user != null && passwordEncoder.matches(request.getPassword(), user.getPassword())) {
 			String token = jwtUtil.generateToken(user.getEmail());
 			UserResponse userResponse = new UserResponse();
 			userResponse.setEmail(user.getEmail());
@@ -44,15 +39,16 @@ public class UserController {
 			userResponse.setToken(token);
 			return ResponseEntity.ok(new ApiResponse<>(true, "Logic success", userResponse));
 		} else {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse<>(false, "Invalid email or password", null));
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+					.body(new ApiResponse<>(false, "Invalid email or password", null));
 		}
 	}
 
-    @PostMapping("/register")
+	@PostMapping("/register")
 	public ResponseEntity<ApiResponse> createUser(@Valid @RequestBody RegisterRequest newUser) {
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
 		newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
-        User user = userService.saveUser(newUser);
+		User user = userService.saveUser(newUser);
 		return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(true, "Register success", user));
 	}
 }
