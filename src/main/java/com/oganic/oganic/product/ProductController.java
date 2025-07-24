073fe575414729919b49deb2c8b7e95ad7c9bf7f
TuanNH <tuannh@gmai.com>
2025-07-24 14:43:36 +0900

    fix: get discount product

--- src/main/java/com/oganic/oganic/product/ProductController.java
+++ src/main/java/com/oganic/oganic/product/ProductController.java
@@ -5,17 +5,13 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.oganic.oganic.category.Category;
 import com.oganic.oganic.category.CategoryService;
 
 // for thymeleaf
@@ -83,11 +79,9 @@ import com.oganic.oganic.category.CategoryService;
 public class ProductController {
 
 	private final ProductService productService;
-	private final CategoryService categoryService;
 
-	ProductController(ProductService productService, CategoryService categoryService) {
+	ProductController(ProductService productService) {
 		this.productService = productService;
-		this.categoryService = categoryService;
 	}
 
 	@GetMapping
@@ -105,8 +99,7 @@ public class ProductController {
 	@GetMapping("/list-products")
 	public ResponseEntity<Page<Product>> getAllProduct(@RequestParam("page") Optional<Integer> page) {
 		Integer pageNumber = page.orElse(1);
-		Page<Product> pageProduct = productService.getProductsPageable(pageNumber - 1, 2);
-
+		Page<Product> pageProduct = productService.getProductsPageable(pageNumber - 1, 3);
 		return ResponseEntity.status(HttpStatus.OK).body(pageProduct);
 	}
 
@@ -114,6 +107,12 @@ public class ProductController {
 	public ResponseEntity<List<Product>> getLatestProducts() {
 		List<Product> latestProducts = productService.getLatestProducts();
 		return ResponseEntity.status(HttpStatus.OK).body(latestProducts);
+	}
+
+	@GetMapping("/discount")
+	public ResponseEntity<List<Product>> getDiscountProducts() {
+		List<Product> discountProducts = productService.getDiscountProducts();
+		return ResponseEntity.status(HttpStatus.OK).body(discountProducts);
 	}
 
 	@GetMapping("/search")
